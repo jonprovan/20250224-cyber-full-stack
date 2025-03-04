dcb415e39dfc5dc6fa5134740a2d0c430bd13633
Jon Provan <jprovan@skillstorm.com>
2025-03-04 14:03:01 -0500

    W2D2 Mid-Afternoon

--- week-02/day-02/food_schema.sql
+++ week-02/day-02/food_schema.sql
@@ -1,14 +1,14 @@
 # A SCHEMA FOR OUR FOOD DATABASE
--- one user can have many recipes
+-- one cook can have many recipes
 -- one recipe can have many ingredients
 -- one ingredient can go in many recipes
 -- one recipe can go in many categories
 -- one category can have many recipes
 -- one recipe can have many ratings
--- one user can have one bio
+-- one cook can have one bio
 
--- user ONE TO ONE bio
--- user ONE TO MANY recipe
+-- cook ONE TO ONE bio
+-- cook ONE TO MANY recipe
 -- recipe ONE TO MANY rating
 -- recipe MANY TO MANY ingredient
 -- recipe MANY TO MANY category
@@ -49,6 +49,52 @@ CREATE TABLE `food`.`cook` (
     REFERENCES `food`.`bio` (`id`)
     ON DELETE SET NULL
     ON UPDATE CASCADE);
+    
+CREATE TABLE `food`.`recipe` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `recipe_name` VARCHAR(64) NOT NULL,
+  `recipe_description` VARCHAR(500) NULL,
+  `cook` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `recipe_cook1_idx` (`cook` ASC) VISIBLE,
+  CONSTRAINT `recipe_cook1`
+    FOREIGN KEY (`cook`)
+    REFERENCES `food`.`cook` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+    
+CREATE TABLE `food`.`recipe_ingredient` (
+  `recipe_id` INT NOT NULL,
+  `ingredient_id` INT NOT NULL,
+  `amount` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`recipe_id`, `ingredient_id`),
+  INDEX `ingredient_id1_idx` (`ingredient_id` ASC) VISIBLE,
+  CONSTRAINT `recipe_id1`
+    FOREIGN KEY (`recipe_id`)
+    REFERENCES `food`.`recipe` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `ingredient_id1`
+    FOREIGN KEY (`ingredient_id`)
+    REFERENCES `food`.`ingredient` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+    
+CREATE TABLE `food`.`recipe_category` (
+  `recipe_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  PRIMARY KEY (`recipe_id`, `category_id`),
+  INDEX `category_id1_idx` (`category_id` ASC) VISIBLE,
+  CONSTRAINT `recipe_id2`
+    FOREIGN KEY (`recipe_id`)
+    REFERENCES `food`.`recipe` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `category_id1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `food`.`category` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
   
 -- to drop a table -- CAREFUL!!!
 -- DROP TABLE ingredient;
@@ -66,9 +112,28 @@ INSERT INTO ingredient(ingredient_name, ingredient_type) VALUES('Cayenne Pepper'
 INSERT INTO category(category_name) VALUES('Entree'), ('Appetizer'), ('Dessert'), ('Beverage'), ('Snack'), ('Soup'), ('Salad');
 
 -- single or double quotes are fine for strings
-INSERT INTO bio(cook_description) VALUES("This is Dave, he cooks with an old Hedley Bennett apron, his Japanese Santoku, and an old restaurant towel in hand.");
+INSERT INTO bio(cook_description) VALUES("This is Dave, he cooks with an old Hedley Bennett apron, his Japanese Santoku, and an old restaurant towel in hand."),
+										('This is Jon. He cannot cook and cannot learn. He is hopeless and shameless.');
 
-INSERT INTO cook(first_name, last_name, bio) VALUES('Dave', 'Lazaro', 1);
+INSERT INTO cook(first_name, last_name, bio) VALUES('Dave', 'Lazaro', 1), ('Jon', 'Provan', 2);
+
+INSERT INTO recipe(recipe_name, recipe_description, cook) VALUES('Marinated Chicken', 'Drool-worthy chicken with flatbread on the side.', 1),
+															    ('Tagliatelle alla Bolognese', 'Italian ragu-style with long pasta.', 1),
+																('Cuban Sandwich', 'Cuban bread cradling pulled pork, sweet ham, mustard and pickles.', 1),
+                                                                ('The Total Mess', 'A whole bunch of stuff, whatever is left in the fridge.', 2);
+                                                                
+INSERT INTO recipe_category(recipe_id, category_id) VALUES(1, 1), (2, 1), (3, 5), (4, 2);
+
+INSERT INTO recipe_ingredient(recipe_id, ingredient_id, amount) VALUES(4, 1, 'Six pounds'), (4, 2, 'A shovelful'), (4, 3, 'One-tenth of a teaspoon'), (4, 4, 'As many as fit');
+
+-- updating a record
+-- this will change EVERY record where the 'where' clause is true, so this is a little unsafe
+-- UPDATE cook SET id = 2 WHERE first_name = 'Dave';
+-- better to do it via id
+UPDATE cook SET id = 1 WHERE id = 2;
+
+-- deleting a record
+DELETE FROM recipe WHERE id = 1;
 
 -- some select statements
 SELECT * FROM ingredient;
@@ -83,6 +148,17 @@ SELECT * FROM category;
 SELECT * FROM bio;
 SELECT * FROM cook;
 SELECT * FROM cook JOIN bio ON cook.bio = bio.id;
+SELECT * FROM recipe;
+SELECT * FROM cook JOIN recipe ON cook.id = recipe.cook;
+SELECT * FROM recipe_category;
+-- joins with multiple tables get complex!
+SELECT recipe_name, recipe_description, category_name FROM recipe 
+	JOIN recipe_category ON recipe.id = recipe_category.recipe_id
+    JOIN category ON category.id = recipe_category.category_id;
+    
+SELECT * FROM recipe 
+	JOIN recipe_ingredient ON recipe.id = recipe_ingredient.recipe_id
+    JOIN ingredient ON ingredient.id = recipe_ingredient.ingredient_id;
 
 
   

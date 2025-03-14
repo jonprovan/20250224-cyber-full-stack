5c2d2178cb89cb2ddb579caa3c58477b63143d63
Jon Provan <jprovan@skillstorm.com>
2025-03-13 22:19:48 -0400

    CORS Update

--- week-02/day-03/food-api/src/main/java/com/skillstorm/food/FoodApiApplication.java
+++ week-02/day-03/food-api/src/main/java/com/skillstorm/food/FoodApiApplication.java
@@ -1,5 +1,8 @@
 package com.skillstorm.food;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 /*
  * this class is the starting point for our application
@@ -35,12 +39,20 @@ public class FoodApiApplication {
 		// disabling Cross-Site Resource Forgery protection for now
 		http.csrf().disable();
 		
+		// bypassing Cross-Origin Resource Sharing protection for now
+		http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(request -> {
+	           CorsConfiguration cc = new CorsConfiguration().applyPermitDefaultValues();
+	           cc.setAllowedMethods(new LinkedList<>(Arrays.asList("GET", "POST", "PUT", "DELETE")));
+	           return cc;
+			})
+	    );
+		
 		http.authorizeHttpRequests(requests -> {
 			
 			// saying whether or not requests of certain methods/endpoints are allowed or denied
 			// once a request matches one of these, top-down, the rest are ignored!
 			requests.requestMatchers(HttpMethod.GET, "/**").permitAll();
-			requests.anyRequest().authenticated();
+			requests.anyRequest().permitAll();
 			
 		});
 		
